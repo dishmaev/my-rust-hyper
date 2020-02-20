33a12f46f8e45bad42650ca30f6d9d8c618070c7
Dmitry Ishmaev <idax@rambler.ru>
2020-02-20 12:17:37 +0300

    remove futures-await-test from dev dependence

--- src/webapi/tests/unit_tests.rs
+++ src/webapi/tests/unit_tests.rs
@@ -1,26 +1,25 @@
 use super::super::handlers;
-use futures_await_test::async_test;
 use hyper::StatusCode;
 
-#[async_test]
+#[tokio::test(threaded_scheduler)]
 async fn test_signin_ok() {
     let resp = handlers::signin().await;
     assert_eq!(resp.status(), StatusCode::OK);
 }
 
-#[async_test]
+#[tokio::test(threaded_scheduler)]
 async fn test_signin_err() {
     let resp = handlers::signin().await;
     assert_ne!(resp.status(), StatusCode::BAD_REQUEST);
 }
 
-#[async_test]
+#[tokio::test(threaded_scheduler)]
 async fn test_signup_ok() {
     let resp = handlers::signin().await;
     assert_eq!(resp.status(), StatusCode::OK);
 }
 
-#[async_test]
+#[tokio::test(threaded_scheduler)]
 async fn test_signup_err() {
     let resp = handlers::signin().await;
     assert_ne!(resp.status(), StatusCode::BAD_REQUEST);
